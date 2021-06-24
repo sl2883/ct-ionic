afab4ef892971ff3871b3950e808e04b0de92874
Sunny <sl2883@cornell.edu>
2021-06-24 10:37:58 -0700

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,5 +42,46 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.clevertap.registerPush();
+
+    // document.addEventListener('onDeepLink', (e: any) => {
+    //   console.log('Clevertap onDeepLink');
+    //   console.log(e.deeplink);
+
+    //   alert('Clevertap onDeepLink' + e.deeplink);
+    // });
+
+    // document.addEventListener('onPushNotification', (e: any) => {
+    //   console.log('onPushNotification');
+    //   console.log(JSON.stringify(e.notification));
+    // });
+
+    // // On success, we should be able to receive notifications
+    // PushNotifications.addListener('registration',
+    //   (token: Token) => {
+    //     alert('Push registration success, token: ' + token.value);
+    //     //this.clevertap.setPushToken(token.value)
+    //   }
+    // );
+
+    // // Some issue with our setup and push will not work
+    // PushNotifications.addListener('registrationError',
+    //   (error: any) => {
+    //     alert('Error on registration: ' + JSON.stringify(error));
+    //   }
+    // );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 }
